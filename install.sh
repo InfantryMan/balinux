cb0f3f1432fe233bd1dc1d20536f54fcee76c6e8
Rustam Migranov <rust-skate@mail.ru>
2017-05-24 15:21:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
-#pros'ba vvesti parol
 #!/bin/bash
-apt-get install apache2 -y
-/etc/init.d/apache2 stop
-apt-get install nginx -y
-apt-get install sysstat -y
-apt-get install bc -y
-mkdir /var/www/html/sysinfo
-cp ./index.cgi /var/www/html/sysinfo/index.cgi
-chmod 755 /var/www/html/sysinfo/index.cgi
-cp ./nginx-sysinfo.conf  /etc/nginx/sites-available/default
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo apt-get install apache2 -y
+sudo /etc/init.d/apache2 stop
+sudo apt-get install nginx -y
+sudo apt-get install sysstat -y
+sudo apt-get install bc -y
+sudo mkdir /var/www/html/sysinfo
+sudo cp ./index.cgi /var/www/html/sysinfo/index.cgi
+sudo chmod 755 /var/www/html/sysinfo/index.cgi
+sudo cp ./nginx-sysinfo.conf  /etc/nginx/sites-available/default
 echo "ServerName balinux.local" >> /etc/apache2/apache2.conf
-cp ./ports.conf /etc/apache2/ports.conf
-cp ./apache2-sysinfo.conf /etc/apache2/sites-available/000-default.conf
-a2ensite 000-default.conf
-a2enmod cgi
-systemctl start apache2
-systemctl restart apache2
-systemctl restart nginx
-touch /var/log/mpstat.log  /var/log/iostat.log  /var/log/tcp.log /var/log/udp.log /var/log/df.log /var/log/network.log
-crontab cron.bak
+sudo cp ./ports.conf /etc/apache2/ports.conf
+sudo cp ./apache2-sysinfo.conf /etc/apache2/sites-available/000-default.conf
+sudo a2ensite 000-default.conf
+sudo a2enmod cgi
+sudo systemctl start apache2
+sudo systemctl restart apache2
+sudo systemctl restart nginx
+sudo touch /var/log/mpstat.log  /var/log/iostat.log  /var/log/tcp.log /var/log/udp.log /var/log/df.log /var/log/network.log
+sudo crontab cron.bak
 
