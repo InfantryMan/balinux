c0b8616ba5fee2b11bce1594662622758378e5a7
Rustam Migranov <rust-skate@mail.ru>
2017-05-24 15:42:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo mkdir /var/www/html/sysinfo
 sudo cp ./index.cgi /var/www/html/sysinfo/index.cgi
 sudo chmod 755 /var/www/html/sysinfo/index.cgi
 sudo cp ./nginx-sysinfo.conf  /etc/nginx/sites-available/default
-echo "ServerName balinux.local" >> /etc/apache2/apache2.conf
+sudo cp ./apache2.conf /etc/apache2/apache2.conf
 sudo cp ./ports.conf /etc/apache2/ports.conf
 sudo cp ./apache2-sysinfo.conf /etc/apache2/sites-available/000-default.conf
 sudo a2ensite 000-default.conf
